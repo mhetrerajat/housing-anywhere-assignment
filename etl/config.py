106f82a0f06e835b19eac0c020d7be88fb40d15b
Rajat Mhetre <mhetrerajat@gmail.com>
2021-12-12 00:04:29 +0530

    feat(etl): add config class

---
+++ etl/config.py
@@ -0,0 +1,26 @@
+import dataclasses
+import os
+from pathlib import Path
+
+__all__ = ["get_config"]
+
+
+@dataclasses.dataclass
+class Config:
+    events_api_host: str = "127.0.0.1"
+    events_api_port: str = "5000"
+
+    data_dir: Path = Path("/tmp/housinganywhere_data/")
+
+    @property
+    def events_api_url(self) -> str:
+        return f"http://{self.events_api_host}:{self.events_api_port}"
+
+
+def get_config() -> Config:
+    config = Config()
+
+    # Create directory if missing
+    os.makedirs(config.data_dir, exist_ok=True)
+
+    return config
