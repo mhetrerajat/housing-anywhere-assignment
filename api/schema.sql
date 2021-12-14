3274f227d990ebe475623a78a7abc661a39d0e4d
Rajat Mhetre <mhetrerajat@gmail.com>
2021-12-14 18:07:58 +0530

    refactor(api): refactor ddl queries

--- api/schema.sql
+++ api/schema.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS raw_events;
 
-CREATE TABLE raw_events (
-    event VARCHAR NOT NULL,
-    time DATETIME NOT NULL,
-    unique_visitor_id VARCHAR NOT NULL,
-    ha_user_id VARCHAR,
-    browser VARCHAR,
-    os VARCHAR,
-    country_code VARCHAR,
-    PRIMARY KEY (event, time, unique_visitor_id)
-);
+CREATE TABLE raw_events ( 
+	event                varchar NOT NULL    ,
+	time                 datetime NOT NULL    ,
+	unique_visitor_id    varchar NOT NULL    ,
+	ha_user_id           varchar(36)     ,
+	browser              varchar(128)     ,
+	os                   varchar(64)     ,
+	country_code         varchar(256)     ,
+	CONSTRAINT PrimaryKey PRIMARY KEY ( event, time, unique_visitor_id )
+ );
